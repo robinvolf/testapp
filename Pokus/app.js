59697ee05216b607789cac20fd6d126e30949810
Norbert Volf <norbert.volf@sap.com>
2021-03-12 12:08:08 +0100

    Render answers

--- Pokus/app.js
+++ Pokus/app.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let questions = [
   {
     question: "Cupidatat id ea irure eu officia?",
@@ -37,35 +39,54 @@ let questions = [
   },
 ];
 
-let questionIndex = 0;
+let globalQuestionIndex = 0;
 
-function setCurrentQuestion() {
+function setCurrentQuestion(questionIndex) {
   let textParagraph = document.getElementById("paragraph-question");
   textParagraph.textContent = questions[questionIndex].question;
 }
 
-function setQuestionNumber(index) {
-  document.getElementById("question-number").textContent = index + 1 + ")";
+function setQuestionNumber(questionIndex) {
+  document.getElementById("question-number").textContent =
+    questionIndex + 1 + ")";
 }
 
-function buttonDisableCheck(index, len) {
-  let lessThanMinimum = index <= 0;
-  let greatherThanMaximum = index >= len - 1;
+function renderQuestions(questionIndex) {
+  let selectedAnswers = questions[questionIndex].answers;
+  let answerHTML = "";
+
+  for (let i = 0; i < selectedAnswers.length; i++) {
+    answerHTML =
+      answerHTML +
+      `<div class="form-check mb-3">"
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+            <label class="form-check-label" for="flexCheckDefault1">${selectedAnswers[i]}</label>
+        </div>`;
+  }
+  document.getElementById("block-answers").innerHTML = answerHTML;
+}
+
+function buttonDisableCheck(questionIndex, questionsLength) {
+  let lessThanMinimum = questionIndex <= 0;
+  let greatherThanMaximum = questionIndex >= questionsLength - 1;
 
   document.getElementById("button-previous").disabled = lessThanMinimum;
   document.getElementById("button-next").disabled = greatherThanMaximum;
 }
 
-function handlerClickNextButton() {
-  setCurrentQuestion(++questionIndex);
+function changeQuestion(questionIndex, questionsLength) {
+  setCurrentQuestion(questionIndex);
   setQuestionNumber(questionIndex);
-  buttonDisableCheck(questionIndex, questions.length);
+  buttonDisableCheck(questionIndex, questionsLength);
+  renderQuestions(questionIndex);
+}
+
+function handlerClickNextButton() {
+  changeQuestion(++globalQuestionIndex, questions.length);
 }
 
 function handlerClickPreviousButton() {
-  setCurrentQuestion(--questionIndex);
-  setQuestionNumber(questionIndex);
-  buttonDisableCheck(questionIndex, questions.length);
+  changeQuestion(--globalQuestionIndex, questions.length);
 }
 
 document
@@ -75,4 +96,4 @@ document
   .getElementById("button-next")
   .addEventListener("click", handlerClickNextButton);
 
-setCurrentQuestion(questionIndex);
+changeQuestion(globalQuestionIndex, questions.length);
