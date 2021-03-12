5545f0512ddfa2c61ae748f2bb8a017e1c9cfce3
Robin Volf <robin.volf@seznam.cz>
2021-03-12 11:32:17 +0100

    render numbers, check button disable limits

--- Pokus/app.js
+++ Pokus/app.js
@@ -8,8 +8,27 @@ let questions = [
       "Anim adipisicing minim cillum aliquip officia esse cillum non sint commodo irure amet aliqua.",
     ],
   },
+  
   {
     question: "Cillum irure et id laboris nulla aliquip esse sint ex nisi.?",
+    answers: [
+      "Veniam deserunt ut laboris deserunt pariatur irure voluptate enim exercitation ad consequat.",
+      "Excepteur fugiat commodo in ullamco minim.",
+      "Do voluptate est do est aliquip incididunt incididunt consequat velit dolor veniam incididunt eiusmod.",
+      "Anim adipisicing minim cillum aliquip officia esse cillum non sint commodo irure amet aliqua.",
+    ],
+  },
+  {
+    question: "Vestibulum tincidunt faucibus placerat.?",
+    answers: [
+      "Veniam deserunt ut laboris deserunt pariatur irure voluptate enim exercitation ad consequat.",
+      "Excepteur fugiat commodo in ullamco minim.",
+      "Do voluptate est do est aliquip incididunt incididunt consequat velit dolor veniam incididunt eiusmod.",
+      "Anim adipisicing minim cillum aliquip officia esse cillum non sint commodo irure amet aliqua.",
+    ],
+  },
+  {
+    question: "Proin dignissim vehicula vulputate.?",
     answers: [
       "Veniam deserunt ut laboris deserunt pariatur irure voluptate enim exercitation ad consequat.",
       "Excepteur fugiat commodo in ullamco minim.",
@@ -26,17 +45,39 @@ function setCurrentQuestion() {
   textParagraph.textContent = questions[questionIndex].question;
 }
 
+function setQuestionNumber() {
+  document.getElementById("question-number").textContent = (questionIndex + 1)+")";
+}
+
+function buttonDisableCheck() {
+  if (questionIndex == 0) {
+    document.getElementById("button-previous").disabled = true;
+    document.getElementById("button-next").disabled = false;
+  }
+
+  else if (questionIndex == questions.length - 1) {
+    document.getElementById("button-next").disabled = true;
+    document.getElementById("button-previous").disabled = false;
+  }
+
+  else {
+    document.getElementById("button-previous").disabled = false;
+    document.getElementById("button-next").disabled = false;
+  }
+}
+
 function handlerClickNextButton() {
   setCurrentQuestion(++questionIndex);
+  setQuestionNumber();
+  buttonDisableCheck();
 }
 
 function handlerClickPreviousButton() {
   setCurrentQuestion(--questionIndex);
+  setQuestionNumber();
+  buttonDisableCheck();
 }
 
-document
-  .getElementById("button-finish")
-  .addEventListener("click", handlerClickNextButton);
 document
   .getElementById("button-previous")
   .addEventListener("click", handlerClickPreviousButton);
